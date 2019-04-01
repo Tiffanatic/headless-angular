ce85c522e70f5273b065ac1a376da12b9b51b530
Tiffanatic <madsrynord@live.dk>
2019-04-01 12:01:35 +0200

    added basic things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'headless-angular';
+export class AppComponent implements OnInit {
+ posts = [];
+
+ constructor(private Http: HttpClient) {
+
+ }
+ ngOnInit(): void {
+   this.Http.get('http://localhost:88/wordpress/wp-json/wp/v2/posts').subscribe(data => {
+     for (let key in data) {
+      if(data.hasOwnProperty(key)) {
+        this.posts.push(data[key]);
+      }
+     }
+     console.log(this.posts);
+   });
+ }
 }
