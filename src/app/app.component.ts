dab019ed093fe13612f5b36558f7c98c149fb0b8
Tiffanatic <madsrynord@live.dk>
2019-04-01 13:10:53 +0200

    start på events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -8,14 +8,20 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
  posts = [];
- postid = null;
+ @Output() postid = new EventEmitter<void>();
  constructor(private Http: HttpClient) {
 
  }
+
+ onSelected(id) {
+  this.postid.emit();
+  console.log(this.postid);
+ }
+
  ngOnInit(): void {
    this.Http.get('http://localhost:88/wordpress/wp-json/wp/v2/posts').subscribe(data => {
      for (let key in data) {
-      if(data.hasOwnProperty(key)) {
+      if (data.hasOwnProperty(key)) {
         this.posts.push(data[key]);
       }
      }
